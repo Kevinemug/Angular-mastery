6ad9295a03ca1a01db38b813460320dbf6de02f3
Kevinemug <kevineeuwamahoro@gmail.com>
2024-03-05 11:51:26 +0200

    feat: control flow

--- projects/04-control-flow-if/src/app/app.component.ts
+++ projects/04-control-flow-if/src/app/app.component.ts
@@ -11,15 +11,20 @@ import { AccountInfo } from './account-info';
       <section class="membership-info">
         <p>
           <!-- name -->
-          {{ '' }}
+          {{ account.name }}
         </p>
-        <p>Valid Thru: {{ '' }}</p>
-        <p>CVV: {{ '' }}</p>
+        <p>Valid Thru: {{ account.validThru }}</p>
+        <p>CVV: {{ account.CVV }}</p>
         <p>
           <!-- membership status -->
-          <span class="badge gold">Gold</span>
-          <span class="badge platinum">Platinum</span>
-          <span class="badge silver">Silver</span>
+          @if (account.membershipStatus === 'gold'){
+            <span class="badge gold">Gold</span>
+          } @else if (account.membershipStatus === 'platinum') {
+            <span class="badge platinum">Platinum</span>
+
+          } @else {
+            <span class="badge silver">Silver</span>
+          }
         </p>
       </section>
     </article>
