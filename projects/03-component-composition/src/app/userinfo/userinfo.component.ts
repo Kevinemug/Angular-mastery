5531b13b05bb168347b46e9e3f79620b767b038a
Kevinemug <kevineeuwamahoro@gmail.com>
2024-03-05 11:50:07 +0200

    feat: building a reusable component

---
+++ projects/03-component-composition/src/app/userinfo/userinfo.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-userinfo',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+ <img src="/assets/noun-pie-chart-6331100-C462DD.png" height="300" />
+  `,
+  styles: ``
+})
+export class UserinfoComponent {
+
+}
