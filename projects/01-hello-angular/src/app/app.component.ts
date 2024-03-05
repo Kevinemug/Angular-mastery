3e5abb18019389a22f2cdd4b7f40e709796d229e
Kevinemug <kevineeuwamahoro@gmail.com>
2024-03-05 11:48:08 +0200

    feat: using templates in angular

--- projects/01-hello-angular/src/app/app.component.ts
+++ projects/01-hello-angular/src/app/app.component.ts
@@ -9,7 +9,17 @@ import { RouterOutlet } from '@angular/router';
   template: `
     <h1>If you are reading this...</h1>
     <p>Things have worked out well! 🎉</p>
+    <ol>
+      <li>Becoming</li>
+      <li>Litttle girls</li>
+      <li>Stand up</li>
+    </ol>
   `,
-  styles: ``,
+  styles: `
+  ol{
+    list-style-type: upper-roman
+
+  }
+  `,
 })
 export class AppComponent {}
