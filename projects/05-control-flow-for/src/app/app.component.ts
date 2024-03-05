74a77fa6bfebc540a820780b9cb9dee503f39532
Kevinemug <kevineeuwamahoro@gmail.com>
2024-03-05 11:51:59 +0200

    feat: control flow with loops

--- projects/05-control-flow-for/src/app/app.component.ts
+++ projects/05-control-flow-for/src/app/app.component.ts
@@ -6,31 +6,31 @@ import { Component } from '@angular/core';
   template: `
     <section class="container">
       <!-- This article element represents and entire listing -->
-      <article class="listing">
+      @for (item of carList; track item){
+        <article class="listing">
         <div class="image-parent">
-          <img class="product-image" src="https://placehold.co/100x100" />
+          <img class="product-image" src="../assets/blue-car.jpeg" />
         </div>
         <section class="details">
-          <p class="title"><!-- car make and model--></p>
+          <p class="title">{{item.make}} {{item.model}}</p>
           <hr />
           <p class="detail">
-            <span>Year</span>
-            <span><!-- year --></span>
+            <span>{{item.year}}</span>
           </p>
           <div class="detail">
-            <span>Transmission</span>
-            <span><!-- transmission --></span>
+            <span>{{item.transmission}}</span>
           </div>
           <p class="detail">
-            <span>Mileage</span>
-            <span><!-- miles --></span>
+            <span>{{item.miles}}</span>
           </p>
           <p class="detail">
-            <span>Price</span>
-            <span><!-- price --></span>
+            <span>{{item.price}}</span>
           </p>
         </section>
       </article>
+      }@empty {
+        <p>No listings available</p>
+      }
     </section>
   `,
   styleUrl: 'app.component.css',
